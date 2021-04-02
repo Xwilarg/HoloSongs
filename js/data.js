778306c17899b75b7d404577c788ac2b15cfa929
Xwilarg <xwilarg@yahoo.fr>
2021-04-03 01:38:11 +0200

    Add songs

--- js/data.js
+++ js/data.js
@@ -1,11 +1,207 @@
 let data = {
+    // JP GEN 0
+    "suisei": {
+        "original": [],
+        "covers": [
+            "yk51RdzaMUE", // 月陽 ツキアカリ (with Kanata)
+        ]
+    },
+
+    // AZKi
+    "azki": {
+        "original": [],
+        "covers": [
+            "YcfZziSg0cg" // 三つ葉の結びめ (with Kanata)
+        ]
+    },
+
     // GAMERS
-    "okayu" : {
+    "fubuki": {
+        "original": [
+            "K84w0Iai40k" // 百花繚乱花吹雪 (with Mio and Ayame)
+        ],
+        "covers": []
+    },
+    "korone": {
+        "original": [],
+        "covers": [
+            "G49bAinM6Lc", // 恋愛裁判
+            "OGYu6Kj3bL0", // スイートマジック
+            "saizawXi4i0", // ハロハロナリヤンス音頭
+            "qgeuRSawFaw" // ボンバーキングのテーマ
+        ]
+    },
+    "mio": {
+        "original": [
+            "x1BGG98tcrg", // 至上主義アドトラック (with Flare and Matsuri)
+            "K84w0Iai40k" // 百花繚乱花吹雪 (with Fubuki and Ayame)
+        ],
+        "covers": [
+            "ESM-OniA96w" // 「おとなの掟」 (with Okayu)
+        ]
+    },
+    "okayu": {
         "original": [
             "j6P_m6adkgc" // もぐもぐYUMMY！
         ],
         "covers": [
-
+            "4muYzftomAE", // flos
+            "9PYi1080Q7k", // 失楽ペトリ
+            "kP08-jP1-7Y", // 乙女解剖
+            "PNA-sTiC0Ds", // ふわふわ時間
+            "dXUJ0IDdX28", // 惑星ループ
+            "ESM-OniA96w", // 「おとなの掟」 (with Mio)
+            "0OtNQEpSeIA" // チューリングラブ (with Aqua)
         ]
-    }
+    },
+
+    // JP GEN 1
+    "haato": {
+        "original": [],
+        "covers": [
+            "CrbMV64PmPU" // さぷらいずぱらだいす！ (with Polka and Nene)
+        ]
+    },
+    "matsuri": {
+        "original": [
+            "x1BGG98tcrg" // 至上主義アドトラック (with Flare and Mio)
+        ],
+        "covers": [
+            "kMPx3AHvwtA", // お願いダーリン
+            "TT6gTs2B1Uw", // Aimer 「カタオモイ」
+            "j84IEsVYGyE", // 白日 (with Rikka)
+            "Z-q5aJm0UJE", // DADDY! DADDY! DO! (with Rikka)
+            "JonQ09L2N0U", // ワタシノテンシ
+            "2ys9IS5r9yA", // 空色デイズ (with Gura)
+            "f4dJFe4MGcs", // KING
+            "1KyLE_Ejf-U", // Preserved Roses (with Rikka)
+            "phGUYXm1e_I" // Colorful World
+        ]
+    },
+    "mel": {
+        "original": [
+            "t1yXDcuwzpY" // 今宵はHalloween Night! (with Shion, Choco and Rushia)
+        ],
+        "covers": []
+    },
+
+    // JP GEN 2
+    "aqua": {
+        "original": [],
+        "covers": [
+            "0OtNQEpSeIA" // チューリングラブ (with Okayu)
+        ]
+    },
+    "ayame": {
+        "original": [
+            "K84w0Iai40k" // 百花繚乱花吹雪 (with Mio and Fubuki)
+        ],
+        "covers": []
+    },
+    "choco": {
+        "original": [
+            "t1yXDcuwzpY" // 今宵はHalloween Night! (with Mel, Shion and Rushia)
+        ],
+        "covers": []
+    },
+    "shion": {
+        "original": [
+            "t1yXDcuwzpY" // 今宵はHalloween Night! (with Mel, Choco and Rushia)
+        ],
+        "covers": []
+    },
+    "subaru": {
+        "original": [
+            "t-Y-UnC2HQo", // 恋の魔法
+            "qrZtEACCwIU" // どぅーまいべすと！歌ってみた！
+        ],
+        "covers": []
+    },
+
+    // JP GEN 3
+    "flare": {
+        "original": [
+            "x1BGG98tcrg" // 至上主義アドトラック (with Matsuri and Mio)
+        ],
+        "covers": []
+    },
+    "marine": {
+        "original": [
+            "e7VK3pne8N4" // Ahoy!! 我ら宝鐘海賊団☆
+        ],
+        "covers": [
+            "JYtt0inzDR4" // BLOODY STREAM
+        ]
+    },
+    "rushia": {
+        "original": [
+            "t1yXDcuwzpY" // 今宵はHalloween Night! (with Mel, Choco and Shion)
+        ],
+        "covers": []
+    },
+
+    // JP GEN 5
+    "botan": {
+        "original": [
+            "3qkXc6zheig" // BLUE CLAPPER (with Lamy, Polka and Nene)
+        ],
+        "covers": []
+    },
+    "kanata": {
+        "original": [],
+        "covers": [
+            "tGXZNHNhI0I", // 馬と鹿
+            "8HylJNyMuzU", // アウトサイダー
+            "FzwYKoKN_KE", // ゴーストルール
+            "-BI2YFYWnjc", // 獣ゆく細道
+            "_kT4a8IzM70", // 秒針を噛む
+            "yk51RdzaMUE", // 月陽 ツキアカリ (with Suisei)
+            "hicAYp7KXKk", // KING
+            "YcfZziSg0cg" // 三つ葉の結びめ (with AZKi)
+        ]
+    },
+    "lamy": {
+        "original": [
+            "3qkXc6zheig" // BLUE CLAPPER (with Botan, Polka and Nene)
+        ],
+        "covers": []
+    },
+    "nene": {
+        "original": [
+            "3qkXc6zheig" // BLUE CLAPPER (with Lamy, Polka and Botan)
+        ],
+        "covers": [
+            "CrbMV64PmPU" // さぷらいずぱらだいす！ (with Polka and Haato)
+        ]
+    },
+    "polka": {
+        "original": [
+            "LQ_eazT56FA", // HOLOGRAM CIRCUS
+            "3qkXc6zheig" // BLUE CLAPPER (with Lamy, Nene and Botan)
+        ],
+        "covers": [
+            "UpoysjZfWrU", // エンヴィーベイビー
+            "NdHPnTd2Jd8", // ルーザーガール
+            "syhlmTNW_a8", // メンヘラじゃないもん！
+            "CrbMV64PmPU" // さぷらいずぱらだいす！ (with Nene and Haato)
+        ]
+    },
+
+    // EN Gen 1
+    "gura": {
+        "original": [],
+        "covers": [
+            "2ys9IS5r9yA", // 空色デイズ (with Matsuri)
+        ]
+    },
+
+    // Holostars GEN 1
+    "rikka": {
+        "original": [],
+        "covers": [
+            "j84IEsVYGyE", // 白日 (with Matsuri)
+            "Z-q5aJm0UJE", // DADDY! DADDY! DO! (with Matsuri)
+            "1KyLE_Ejf-U" // Preserved Roses (with Matsuri)
+        ]
+    },
 }
