f4a968ceeac5ac6260293009322b576f13b844a4
Xwilarg <xwilarg@yahoo.fr>
2021-04-06 21:16:34 +0200

    Fix data not loading from secure page

--- js/songs.js
+++ js/songs.js
@@ -16,7 +16,7 @@ function toggle(id,name) {
 function getVideoHtml(e, name, className) {
     return `
         <div>
-            <img id="img_` + e + `_` + name + `" data-src="http://img.youtube.com/vi/` + e + `/mqdefault.jpg" onclick="toggle('` + e + `', '` + name + `')" class="lazyload"/>
+            <img id="img_` + e + `_` + name + `" data-src="https://img.youtube.com/vi/` + e + `/mqdefault.jpg" onclick="toggle('` + e + `', '` + name + `')" class="lazyload"/>
             <iframe id="iframe_` + e + `_` + name + `" class="hidden"></iframe>
         </div>
     `;
